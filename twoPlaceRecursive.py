96e0be03488d4fe1eb2fed679be011dbefe522b4
Victor Taksheyev <victortaksheyev@gmail.com>
2019-06-03 12:26:24 -0700

    Cleaned up code

--- twoPlaceRecursive.py
+++ twoPlaceRecursive.py
@@ -1,56 +1,40 @@
 from __future__ import print_function
-wff = "a>b>c>d"
-# wff = "a"
-wff2 = "a"
+wff = "a>bvc>d>e"
 
 # for each connective
-connectives = '>'
+connectives = ['>', 'v']
 
 f = open("tenOperators.txt", "w+")
 
 # returns index of wff
-def findConnective(wff, indexes):
-    if len(wff) == None:
-        return -1
-    if (len(wff) <= 1):
-        return -1                                   # it's an atomic
-
-    for i in range(len(wff)):                       # looping through all chars in wff
-        # print("we're inside")
+def findConnective(wff, indexes):                             
+    for i, char in enumerate(wff):                      # looping through all chars in wff
         if set([i]) & set(indexes):                     # if operator has already been used
             continue
         else:                                           # if operator has not been usedl
-            for j in range(len(connectives)):           # looping through all of the connectives
-                if wff[i] == connectives[j]:            # if the wff contains the connective
-                    indexes.append(i)                   # keeps track of which operators have already been used
-                    return i
+            if char in connectives:                     # if the wff contains the connective
+                indexes.append(i)                       # keeps track of which operators have already been used
+                return i
 
 # returns number of connectives
 def numConnectives(wff):
     count = 0
     for c in wff:
-        if c == connectives:
+        if c in connectives:
             count += 1
     return count
 
-# for i in range(numConnectives(wff)):
-
 # returns what's on left of operator
 def createLeft(wff, opIndex):
-    if opIndex == -1:
-        return wff          # return the atomic
-    else:
-        return wff[:opIndex]
+    return wff[:opIndex]
 
 # returns what's on right of operator
 def createRight(wff, opIndex):
-    if opIndex == -1:
-        return wff          # return the atomic
-    else:
-        return wff[opIndex+1:]
+    return wff[opIndex+1:]
 
+# recursive algorithm
 def rec(wff):
-    ind = []                                        # list storing indexes of operators used
+    ind = []                                        # storing indexes of operators used
     if len(wff) == 1:
         yield wff
         return 
@@ -64,10 +48,5 @@ def rec(wff):
                     yield "(" + left_subexpr + ")" + wff[opIndex] + "(" + right_subexpr + ")"
 
 result = rec(wff)
-
-count = 0
 for index, x in enumerate(result):
-    count += 1
-
-    print(str(index+1).ljust(4) + "  " + x + "\n")
-print(count)
+    print(index, x)
